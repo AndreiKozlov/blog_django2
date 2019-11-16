3ed483a112afd9e31a9569b7f49010a85576f04a
AndreiKozlov <torg-net@mail.ru>
2019-11-17 04:24:04 +0500

    17.11.2019

--- appblog/models.py
+++ appblog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-#
-# class Category(models.Model):
-#     name = models.CharField(max_length=255, verbose_name='')
-#     slug =
+
+class Category(models.Model): # Таблица категорий в БД
+    name = models.CharField('Имя категории', max_length=255) #max_length - макс. 255
+    slug = models.SlugField('Url', max_length=255) #ссылка категории (url) сделать в автомате по имени категории!!!
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
